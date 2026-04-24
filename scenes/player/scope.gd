8a905f7056f8233dd19ca3f087d62fae6fc4b530
whangb11 <71579712+whangb11@users.noreply.github.com>
2026-04-24 21:30:32 +0800

    b

--- scenes/player/scope.gd
+++ scenes/player/scope.gd
@@ -1,5 +1,8 @@
 extends MeshInstance3D
 
+@onready var camera:Camera3D = $TargetViewport/Camera
+@onready var view_area:MeshInstance3D = $ViewArea
+@onready var viewport:SubViewport = $TargetViewport
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
